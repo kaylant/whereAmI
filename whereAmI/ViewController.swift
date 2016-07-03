512647c7f4d1f34a4a1afab5701d9089011b8c2b
kaylant <kaylant@gmail.com>
2016-07-03 16:07:00 -0500

    finished gathering user's location info

--- whereAmI/ViewController.swift
+++ whereAmI/ViewController.swift
@@ -12,7 +12,14 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var manager:CLLocationManager!
-
+    
+    @IBOutlet var latitudeLabel: UILabel!    
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    @IBOutlet var speedLabel: UILabel!
+    @IBOutlet var altitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,11 +32,59 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     
         print(locations)
         
+        var userLocation:CLLocation = locations[0]
+        
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+        self.courseLabel.text = "\(userLocation.course)"
+        
+        self.speedLabel.text = "\(userLocation.speed)"
+        
+        self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        // for address:
+        // geocoding is process of going from place name to coordinates, 
+        // reverse is going from latitude/longitude to an address
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:  { (placemarks, error) -> Void in
+            
+            // first check for error
+            
+            if (error != nil) {
+                
+                print(error)
+                
+            } else {
+            
+                if placemarks!.count > 0 {
+                    // create a placemark
+                    //  testing to see if we can convert the first return in an array (location)
+                    let p = (placemarks![0])
+                        
+                    if (p.subThoroughfare != nil) {
+                        
+                        let subThoroughFare = p.subThoroughfare
+                
+                        self.addressLabel.text = "\(subThoroughFare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \n \(p.country)"
+                        
+                    }
+                        
+                    
+                    
+                }
+            
+            }
+            
+        })
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
